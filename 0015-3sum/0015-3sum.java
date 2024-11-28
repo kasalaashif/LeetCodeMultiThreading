cafd7fe2b869bd04197a7eba6620873a241d6fa1
Ashif Kasala <98870453+kasalaashif@users.noreply.github.com>
2024-11-28 15:29:45 +0530

    Time: 31 ms (55.52%), Space: 51.4 MB (53.97%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,29 +2,28 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> triplets = new ArrayList<>();
         Arrays.sort(nums);
-        int length= nums.length;
-        for(int i=0; i<length-2; i++)
+        for(int i=0; i<nums.length-2; i++)
         {
             if(i>0 && nums[i]==nums[i-1]) continue;
             int j=i+1;
-            int k=length-1;
+            int k=nums.length-1;
             while(j<k)
             {
-                int sum =nums[i]+nums[j]+nums[k];
+                int sum = nums[i] + nums[j] + nums[k];
                 if(sum==0)
                 {
-                    triplets.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                    while(j<k&& nums[j]==nums[j+1]) 
+                    triplets.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    while(j<k&&nums[j]==nums[j+1])
                         j++;
-                    while(j<k&& nums[k]==nums[k-1]) 
+                    while(j<k&&nums[k]==nums[k-1])
                         k--;
                     j++;
                     k--;
                 }
-                else if (sum<0)         
-                    j++;
-                else
+                else if(sum>0)
                     k--;
+                else
+                    j++;
             }
         }
         return triplets;
